9e5325183a17d814e111b85006793b3c291fca57
m-arsyad <marsyad.dev@outlook.com>
2021-01-17 07:28:27 +0700

    Add GameStoreMain.java

--- GameStoreCode/src/GameStoreMain.java
+++ GameStoreCode/src/GameStoreMain.java
@@ -1,4 +1,4 @@
-public class App {
+public class GameStoreMain {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Tes");
