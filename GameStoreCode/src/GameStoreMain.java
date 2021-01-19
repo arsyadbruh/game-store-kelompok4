3ca2302ad24c545e7e2ea4c5801d09213d96b65f
m-arsyad <marsyad.dev@outlook.com>
2021-01-19 17:43:08 +0700

    add saldo and exception handler

--- GameStoreCode/src/GameStoreMain.java
+++ GameStoreCode/src/GameStoreMain.java
@@ -4,7 +4,6 @@
  * Dibuat dengan java 15 dan javaFX 8
  */
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -18,20 +17,25 @@ public class GameStoreMain extends Application{
     
     @Override
     public void start(Stage primaryStage) throws Exception{
-        // load login layout
-        FXMLLoader loadLayout = new FXMLLoader(getClass().getResource("view/loginLayout.fxml"));
-        AnchorPane loginPage = (AnchorPane) loadLayout.load();
-        Stage dialogStage = new Stage();
-        dialogStage.setTitle("GameStore | Login");
-        dialogStage.initModality(Modality.WINDOW_MODAL);
-        dialogStage.setResizable(false);
-        Scene scene = new Scene (loginPage);
-        dialogStage.setScene(scene);
+        try {
+            // load login layout
+            FXMLLoader loadLayout = new FXMLLoader(getClass().getResource("view/loginLayout.fxml"));
+            AnchorPane loginPage = (AnchorPane) loadLayout.load();
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("GameStore | Login");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.setResizable(false);
+            Scene scene = new Scene (loginPage);
+            dialogStage.setScene(scene);
 
-        // login controller
-        LoginController control = loadLayout.getController();
-        control.setDialogStage(dialogStage);
-        dialogStage.show();
+            // login controller
+            LoginController control = loadLayout.getController();
+            control.setDialogStage(dialogStage);
+            dialogStage.show();
+        } catch (Exception e) {
+            System.out.println("Error cause by : "+ e.getCause());
+            System.out.println("Error Message : "+e.getMessage());
+        }
         
     }
 
