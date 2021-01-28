6a1463fcd43264ae026b20111fa9e266bed42df9
matalaric <ciralatam@gmail.com>
2021-01-28 08:57:41 +0700

    connect library ke database

--- GameStoreCode/src/controller/UserLibraryController.java
+++ GameStoreCode/src/controller/UserLibraryController.java
@@ -1,5 +1,11 @@
 package controller;
 
+import java.io.InputStream;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import core.ConnectDB;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,11 +20,35 @@ public class UserLibraryController {
 
     @FXML
     private Button backButton;
+    @FXML
+    private Pane paneOne;
 
-    private int user;
+    @FXML
+    private Pane paneTwo;
+
+    @FXML
+    private ImageView thumbOne;
+
+    @FXML
+    private ImageView thumbTwo;
+
+    @FXML
+    private Button DelButtonOne;
+
+    @FXML
+    private Button DelButtonTwo;
+
+private ArrayList<Integer> id_games = new ArrayList<Integer>();
+
+    private int user,id_library;
+
 
     public void setUser(int id_user){
         this.user = id_user;
+        getLibrary();
+        for (Integer i : id_games) {
+            setImgView(i);
+        }
     }
 
     void setDialogStage(Stage dialogStage) {
@@ -59,4 +89,51 @@ public class UserLibraryController {
             System.out.println("Error Message : " + e.getMessage());
         }
     }
+
+    @FXML
+    void handleDelete(ActionEvent event) throws Exception {
+        System.out.println("Delete button");
+        
+        if (event.getSource() == DelButtonOne){
+            paneOne.setVisible(false);
+                
+        }
+
+        if (event.getSource() == DelButtonTwo){
+            paneTwo.setVisible(false);
+        }
+    }
+
+    private void setImgView(int indexPath) throws Exception {
+        String path = Integer.toString(indexPath);
+        try {
+            if (indexPath == 1){
+                InputStream inputStream = getClass().getResourceAsStream("../img/Library/thumbnail/" + path + ".jpg");
+                Image thumbs = new Image(inputStream);
+                thumbOne.setImage(thumbs);
+            }else{
+                InputStream inputStream = getClass().getResourceAsStream("../img/Library/thumbnail/" + path + ".jpg");
+                Image thumbs = new Image(inputStream);
+                thumbTwo.setImage(thumbs);
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Set Image View");
+            System.out.println("Error cause by : " + e.getCause());
+            System.out.println("Error Message : " + e.getMessage());
+        }
+    }
+    private void getLibrary() throws Exception {
+        String query = "SELECT * FROM library WHERE id_user = ?";
+        PreparedStatement stmt = ConnectDB.connect().prepareStatement(query);
+        stmt.setInt(1, user);
+        ResultSet rs = stmt.executeQuery();
+        while(rs.next()) {
+            System.out.println("ada game");
+            int game = rs.getInt("id_game");
+            this.id_games.add(game);
+        }
+
+    }
+
 }
